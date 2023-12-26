56248965578785859d95788a4ab92bea4f8283ad
Артур <informal201144@gmai;.com>
2023-12-26 13:55:49 +0500

    added permission is active and IsAuthenticated permission

--- electronics_network/views.py
+++ electronics_network/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 
 from electronics_network.models import Node
 from electronics_network.paginators import NodePaginator
+from electronics_network.permission import IsActiveUser
 from electronics_network.serializers import NodeSerializer
 
 
@@ -11,7 +12,7 @@ class NodeListApiView(generics.ListAPIView):
     queryset = Node.objects.all()
     serializer_class = NodeSerializer
     pagination_class = NodePaginator
-
+    permission_classes = [IsActiveUser]
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ["contact__country"]
 
@@ -19,22 +20,25 @@ class NodeListApiView(generics.ListAPIView):
 class NodeCreateApiView(generics.CreateAPIView):
     """Создание узла с поставщиком"""
     serializer_class = NodeSerializer
+    permission_classes = [IsActiveUser]
 
 
 class NodeRetrieveAPIView(generics.RetrieveAPIView):
     """Просмотр узла с поставщиком"""
-
     queryset = Node.objects.all()
     serializer_class = NodeSerializer
+    permission_classes = [IsActiveUser]
 
 
 class NodeUpdateApiView(generics.UpdateAPIView):
     """Редактирование узла с поставщиком"""
     queryset = Node.objects.all()
     serializer_class = NodeSerializer
+    permission_classes = [IsActiveUser]
 
 
 class NodeDestroyAPIView(generics.DestroyAPIView):
     """Удаление узла с поставщиком"""
     queryset = Node.objects.all()
     serializer_class = NodeSerializer
+    permission_classes = [IsActiveUser]
